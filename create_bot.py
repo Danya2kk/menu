8cc3954943518ac4906f226c72e9849e09f61296
Danya2kk <90967346+Danya2kk@users.noreply.github.com>
2022-05-07 14:43:38 +0300

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-API_TOKEN = '5325841407:AAFVaKNtblDRE1czMfOfx4X5TfohmRksYNA'
+API_TOKEN = ''
 
 
 storage = MemoryStorage()
